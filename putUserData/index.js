8e9beeb096c3f8cf58694e4998289699bab23c7a
Juan Trejo <25491282+jt113@users.noreply.github.com>
2021-08-23 00:09:36 -0400

    part ii

--- putUserData/index.js
+++ putUserData/index.js
@@ -6,21 +6,35 @@ exports.handler = async (event, context) => {
     const ddb = new AWS.DynamoDB({apiVersion: "2012-08-10"});
     const documentClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"})
 
+    let responseBody = "";
+    let statusCode = 0;
+    const { id, firstName, lastName } = JSON.parse(event.body);
     const params = {
         TableName: "Users",
         Item: {
-            id: "90000",
-            firstName: "peter",
-            lastName: "griffin"
+            id: id,
+            firstName: firstName,
+            lastName: lastName
         }
     }
     try {
        const data = await documentClient.put(params).promise();
-       console.log(data);
+       responseBody = JSON.stringify(data); // data.Item is how ddb formats  responses. contains user data
+       statusCode = 201;
 
     }
     catch(err){
-        console.log(err);
+        responseBody = `Unable to put user data`;
+        statusCode = 403;
 
     }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myheader": "test"
+        },
+        body: responseBody
+    }
+    return response;
 }
