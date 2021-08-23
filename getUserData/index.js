259d61389b1a6e30e50d6b9f3994c4ea9a039607
Juan Trejo <juantrj4@gmail.com>
2021-08-22 20:07:42 -0400

    part II getUserData

--- getUserData/index.js
+++ getUserData/index.js
@@ -7,18 +7,34 @@ exports.handler = async (event, context) => {
     // unmarshall ddb json to std json
     const documentClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"}) 
 
+    let responseBody = "";
+    let statusCode = 0;
+
+    const { id } = event.pathParameters;
+
     const params = {
         TableName: "Users",
         Key: {
-            id: "12345"
+            id: id
         }
     }
     try{
        const data = await documentClient.get(params).promise();
-       console.log(data);
+       responseBody = JSON.stringify(data.Item); // data.Item is how ddb formats  responses. contains user data
+       statusCode = 200;
     }
     catch(err){
-        console.log(err);
+        responseBody = `Unable to get user data`;
+        statusCode = 403;
 
     }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myheader": "test"
+        },
+        body: responseBody
+    }
+    return response;
 }
